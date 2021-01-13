e5e13f2298a50d804604a3006da5a8ee5cb60884
Jaron-Lane <jaronwohlwend@icloud.com>
2021-01-13 13:19:17 -0600

    posting to postman for animals, locations, employees, and customers

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,9 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal
-from locations import get_all_locations, get_single_location
-from customers import get_all_customers, get_single_customer
-from employees import get_all_employees, get_single_employee
+from locations import get_all_locations, get_single_location, create_location
+from customers import get_all_customers, get_single_customer, create_customer
+from employees import get_all_employees, get_single_employee, create_employee
 
 
 # Here's a class. It inherits from another class.
@@ -113,6 +113,12 @@ class HandleRequests(BaseHTTPRequestHandler):
         
         if resource == "locations":
             new_resource = create_location(post_body)
+
+        if resource == "employees":
+            new_resource = create_employee(post_body)
+
+        if resource == "customers":
+            new_resource = create_customer(post_body)
 
         # Encode the new animal and send in response
         self.wfile.write(f"{new_resource}".encode())
